4b26dcf49e9c32b0b7beeddcc64fd31eedf8df55
Dhirubhai <146410587+Dhirubhaidhanush@users.noreply.github.com>
2026-02-07 16:57:28 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -43,6 +43,14 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     allowedHosts: true,
+
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+
     fs: {
       strict: true,
       deny: ["**/.*"],
